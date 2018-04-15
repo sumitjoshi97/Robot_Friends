1cd002da3cd0f18413858ea3c12bcab275be11f9
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-04-15 20:41:12 +0530

    added card component

---
+++ src/Card.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+const Card = () => {
+    return (
+        <div>
+            <img src="https://robohash.org/sumit" alt="Jane"/>
+            <div>
+                <h2>Jane Doe</h2>
+                <p>@gmail.com</p>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
+import Card from './Card';
+import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
+ReactDOM.render(<Card />, document.getElementById('root'));
+registerServiceWorker();
